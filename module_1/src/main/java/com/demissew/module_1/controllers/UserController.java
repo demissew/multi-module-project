4d7d69fa978a3affa9447331483fec1f0d1ccadd
Demissew (Home) <dhex30@gmail.com>
2021-05-25 04:37:38 +0300

    Initial commit

---
+++ module_1/src/main/java/com/demissew/module_1/controllers/UserController.java
@@ -0,0 +1,8 @@
+package com.demissew.module_1.controllers;
+
+import com.demissew.module_2.entities.User;
+
+public class UserController {
+
+    private User user;
+}
---
+++ module_2/src/main/java/com/demissew/module_2/entities/User.java
@@ -0,0 +1,12 @@
+package com.demissew.module_2.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+}
